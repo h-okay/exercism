8a2f70d60e7ebba3b03f48845225b8979e432c2f
Hakan Okay <hokay.ca@gmail.com>
2024-03-08 17:24:09 -0500

    added golang luhn exercise

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,7 +1,36 @@
 package luhn
 
-import "strings"
+import (
+	"strings"
+	"unicode"
+)
 
 func Valid(id string) bool {
-	strings.ToLower(id)
+	id = strings.ReplaceAll(id, " ", "")
+	if len(id) <= 1 {
+		return false
+	}
+
+	sum := 0
+	count := 0
+	for i := len(id) - 1; i >= 0; i-- {
+		if !unicode.IsDigit(rune(id[i])) {
+			return false
+		}
+
+		digit := int(id[i] - '0')
+		if count%2 != 0 {
+			doubled := digit * 2
+			if doubled > 9 {
+				doubled -= 9
+			}
+			sum += doubled
+			count++
+			continue
+		}
+		sum += digit
+		count++
+	}
+
+	return sum%10 == 0
 }
